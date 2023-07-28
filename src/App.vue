22ca64561c9139c9a8d0219be98a2e6078224240
Ramiro Cowdin <echo990311@163.com>
2023-07-28 13:30:14 +0800

    add tokeUserinfo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <script setup>
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import { onMounted } from "vue";
-import { getToken } from "@/apis/index.js";
+import { getToken, getTokenuser } from "@/apis/index.js";
 import { useStore } from "@/stores/user.js";
 import { getCommunity, getTreeMenu, getVillage } from "@/apis/dict.js";
 import { useDict } from "@/stores/dict.js";
-// import { useStore } from './stores/user';
-// const store = useStore()
-// const query = new URLSearchParams(location.href.split('?')[1])
-// const token = query.get('token')
-// 判断 `url` 是否携带 `token`
-// 如果携带，则存储到全局状态，同时存储到本地缓存
-// 如果未携带，则从本地缓存读取，读取成功时存储到全局状态
-// if (token) {
-//   store.setToken(token)
-//   sessionStorage.setItem('token', token)
-// } else {
-//   const localToken = sessionStorage.getItem('token') || ''
-//   if (localToken) {
-//     store.setToken(localToken)
-//   }
-// }
+
 const store = useStore()
 const dict = useDict()
 onMounted(async () => {
+  if (store.userInfo.realName) return
+  const query = new URLSearchParams(location.href.split('?')[1])
+  const token = query.get('token')
+  if (token) {
+    const user = await getTokenuser(token)
+    store.setUserInfo(user.data)
+  }
   if (store.token) return
   const {data} = await getToken()
   store.setToken(data.token)
